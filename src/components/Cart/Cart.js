01b2a9eae77137a68593ade09ae6a328d672c9b4
Uzair <uhass924@gmail.com>
2022-12-03 23:32:45 -0500

    Added a Checkout Cart Modal with the sample layout of: Total items, Total Price, Cancel button, Order button

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,28 +1,28 @@
 import React from "react";
+import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
 
 const Cart = (props) => {
   const cartItems = (
     <ul className={classes["cart-items"]}>
       {[{ id: "cq", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
-        <li>{item.name}</li>
+        <li key={item.id}>{item.name}</li>
       ))}
     </ul>
   );
 
   return (
-    <div>
+    <Modal>
       {cartItems}
       <div className={classes.total}>
-        <span>totalamount</span>
-        <span>35.54</span>
+        <span>Total amount</span>
+        <span>$35.54</span>
       </div>
       <div className={classes.actions}>
         <button className={classes['button--alt']}> Close </button>
         <button className={classes.button}>Order Now</button>
-        action buttons
         </div>
-    </div>
+    </Modal>
   );
 };
 
---
+++ src/components/UI/Modal.js
@@ -0,0 +1,30 @@
+import React from "react";
+// ReactDOM.createPortal exists in the ReactDOM library 
+
+// MAKE SURE TO IMPORT REACTDOM from "react-dom" and not "react"
+import ReactDOM from "react-dom";
+import classes from "./Modal.module.css";
+
+const Backdrop = (props) => {
+  return <div className={classes.backdrop}></div>;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div> {props.children} </div>
+    </div>
+  );
+};
+
+const PortalElement = document.getElementById('overlay');
+
+const Modal = (props) => {
+  return <div>
+    {/* createPortal needs to know not only what to Portal but also WHERE TO Portal ! */}
+    {ReactDOM.createPortal(<Backdrop />, PortalElement)}
+    {ReactDOM.createPortal(<ModalOverlay>{props.children}</ModalOverlay>, PortalElement)}
+  </div>;
+};
+
+export default Modal;
